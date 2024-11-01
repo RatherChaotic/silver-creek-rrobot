cc0d46cecfd9f81565caccb078fd3774bf04e478
RatherChaotic <OccationallyChaotic@outlook.com>
2024-11-01 10:05:17 -0400

    Working on refactoring

--- robot.py
+++ robot.py
@@ -1,1 +1,20 @@
-# TODO: insert robot code here
+import rev, wpilib
+import wpilib.drive
+
+
+class RRobot(wpilib.TimedRobot):
+    def __init__(self):
+        super().__init__()
+
+
+        #Joystick Declaration
+        self.joystick = wpilib.XboxController(0)
+
+        #Motor Declaration
+        self.lf_motor = rev.CANSparkMax(4, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.lr_motor = rev.CANSparkMax(3, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.rf_motor = rev.CANSparkMax(9, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.rr_motor = rev.CANSparkMax(2, rev.CANSparkLowLevel.MotorType.kBrushed)
+
+    def robotInit(self):
+        return None
